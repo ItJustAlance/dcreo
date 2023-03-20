0c04ce22b563e44823ef5ef059658e58ac6f6a0f
ratamanov <r399055750@gmail.com>
2023-03-20 18:52:34 +0500

    попапы

--- js/script.js
+++ js/script.js
@@ -43,38 +43,15 @@ $(function() {
     $(".nav").slideToggle();
   });
   
-  
-  $(".js-project-open").on("click", function() {
-    $("#popup-project").addClass("active");
-    $("body").addClass("body-lock")
-  });
-  $(".js-popup-close").on("click", function() {
-    $(".b-popup").removeClass("active");
-    $("body").removeClass("body-lock")
-  });
-    $(".listing-news .b-news").on("click", function() {
-      $(this).toggleClass("active");
-      $(this).next().slideToggle();
-    });
-	
     if ($('.phone_mask').length) {
         $(".phone_mask").mask("+7 (999) 999-9999");
     }
+
+  $(".js-btn-popup-request").on("click", function (){
+    $("#popup-request").addClass("active");
+    return false;
+  })
   
-    
-    $(".js-toggle-title").on("click", function (){
-      $(this).next(".js-toggle__toggle").slideToggle()
-    })
-  $(".js-show-project").on("click", function (){
-    $(".js-show-project__wrapper").addClass("show");
-    $(this).parent(".b-btn__show-project").fadeOut();
-    return false;
-  })
-  $(".js-client-etc__btn").on("click", function (){
-    $(".js-client-etc__wrapper").addClass("show");
-    $(this).parent(".b-client-etc").fadeOut();
-    return false;
-  })
   $(".js-popup-close").on("click", function() {
     $(".b-popup").removeClass("active");
     $("body").removeClass("body-lock")
